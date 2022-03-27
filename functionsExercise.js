b6802765e5b8522920d11075055acb8a8b3a289d
“username” <angeljunotruffles@gmail.com>
2022-03-27 05:43:28 -0400

    do this bonus, but also ENJOY YOUR BREAK

---
+++ functionsExercise.js
@@ -0,0 +1,44 @@
+function fozzieBear(){
+    console.log(`Wocka wocka!`);
+};
+fozzieBear();
+
+function beaker(speak){
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+};
+beaker(`Meep`);
+
+function muppetShow(a, b){
+    if (a === `Muppet` && b === `Show`){
+        console.log(`It's time to play the music. It's time to light the lights.`);
+    }
+};
+muppetShow(`Muppet`, `Show`);
+
+function kermit(){
+    return `Kermit The Frog`;
+};
+console.log(kermit());
+
+function muppetShowSeason(seasons){
+    if (seasons === 5){
+        return true;
+    } else {
+        return false;
+    };
+};
+console.log(muppetShowSeason(5));
+
+const manOrMuppet = function (){
+    console.log(`Am I a woman or am I am Muppet?`);
+};
+manOrMuppet();
+
+rainbowConnection = () => console.log(`Someday we'll find it.  The rainbow connection. The lovers, the dreamers, and me.`);
+rainbowConnection();
+
+// 8. no
+// 9. yes
